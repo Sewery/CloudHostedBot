c807746628ecb8e1097d228bce3ac2bf74eb11a8
Sewery <seweryn.tasior@op.pl>
2021-01-03 19:04:34 +0100

    Corrected token

--- Przydzielak.java
+++ Przydzielak.java
@@ -9,7 +9,7 @@ public class Przydzielak {
 
 
     public static void main(String[] args) throws LoginException {
-        JDA jda = JDABuilder.createDefault("Nzk0ODc5MTU4MjYxMTIxMDM2.X_BO5w.o5denzKXq9y1GIGnxL7zYPrm1y8").build();
+        JDA jda = JDABuilder.createDefault("Nzk0ODc5MTU4MjYxMTIxMDM2.X_BO5w.4JyH_vEPCbVPIdcKpE3fy0KCyew").build();
             jda.addEventListener(new Autorole());
 
         }
